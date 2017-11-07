7c671f63480d56ac850ce8d821cd5c34eaa7aecc
lyx <563150079@qq.com>
2017-11-07 13:39:15 -0500

    11.07

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from Crypto.Cipher import AES
 from Crypto.Util import Counter
 from Crypto import Random
 import os
-counter = os.urandom(16)
-print([hex(i) for i in counter])
+import Crypto
+print(Crypto.__file__)
